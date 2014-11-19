cfcd328693c1a94056ac394c825c2433c795d5ed
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2014-11-19 20:15:37 +0100

    Remove not properlly working Suse

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -31,12 +31,6 @@ end
 
 unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
 
-  hosts.each do |host|
-    if fact('osfamily') == 'Suse'
-      shell 'cp -f /etc/resolv.conf.netconfig /etc/resolv.conf'
-    end
-  end
-
   # This will install the latest available package on el and deb based
   # systems fail on windows and osx, and install via gem on other *nixes
   foss_opts = { :default_action => 'gem_install' }
@@ -55,7 +49,7 @@ unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
   end
 end
 
-UNSUPPORTED_PLATFORMS = ['AIX','windows','Solaris']
+UNSUPPORTED_PLATFORMS = ['AIX','windows','Solaris', 'Suse']
 
 RSpec.configure do |c|
   # Project root
